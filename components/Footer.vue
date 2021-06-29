6f4aaba8fd671e41858cb9f04e53bb26f6699fa5
Francis Badasu <nytro04@gmail.com>
2021-06-29 08:06:30 +0000

    feat: add footer

---
+++ components/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="flex justify-between footer">
+    <p class="">
+      <a
+        class="move-r"
+        href="https://github.com/nytro04"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        https://github.com/nytro04
+      </a>
+    </p>
+    <p class="">
+      <a
+        class="move-r"
+        href="https://www.linkedin.com/in/francis-badasu"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        https://www.linkedin.com/in/francis-badasu
+      </a>
+    </p>
+    <small
+      >Designed by
+      <a
+        class="underline move-r"
+        href="https://uxkafui.co"
+        target="_blank"
+        rel="noopener noreferrer"
+        >this guy
+      </a>
+    </small>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+.footer {
+  margin: 0 -7.5rem;
+  padding: 0 3rem;
+  background-color: #000;
+  height: 10vh;
+}
+</style>
